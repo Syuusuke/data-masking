2da7c5b0954093325c260c496556578fa548a3c9
hexiang <hexiang@dding.cn>
2019-06-27 01:19:17 +0800

    add data masking

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @return {string}
+ */
+function masking(str, beginLen, endLen, replaceStr = '*') {
+  const firstStr = str.slice(0, beginLen);
+  const middleStr = str.slice(beginLen, endLen).replace(/[\s\S]/ig, replaceStr);
+  const lastStr = str.slice(endLen);
+
+  return `${firstStr}${middleStr}${lastStr}`;
+}
+
+function show(str, beginLen, endLen, replaceStr = '*') {
+  const firstStr = str.slice(0, beginLen).replace(/[\s\S]/ig, replaceStr);
+  const middleStr = str.slice(beginLen, endLen);
+  const lastStr = endLen ? str.slice(endLen).replace(/[\s\S]/ig, replaceStr) : '';
+
+  return `${firstStr}${middleStr}${lastStr}`;
+}
+
+module.exports = { masking, show };
---
+++ test/index.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+
+const { masking, show } = require('../index.js');
+
+describe('index.js', () => {
+  describe('#masking()', () => {
+    it('should 176****0926', () => {
+      const res = masking('17673040926', 3, 7);
+      assert.equal(res, '176****0926');
+    });
+
+    it('should 176****0926', () => {
+      const res = masking('17673040926', -1, 11);
+      assert.equal(res, '1767304092*');
+    });
+  });
+
+  describe('#show()', () => {
+    it('should 张**', () => {
+      const res = show('张三丰', 0, 1);
+      assert.equal(res, '张**');
+    });
+
+    it('should **丰', () => {
+      const res = show('张三丰', -1);
+      assert.equal(res, '**丰');
+    });
+
+    it('should 张##', () => {
+      const res = show('张三丰', 0, 1, '#');
+      assert.equal(res, '张##');
+    });
+
+  });
+});
